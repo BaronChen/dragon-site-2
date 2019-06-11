6ca66ee59aa71b8acb59798b6754dd890afcd3f6
Baron Chen <baron.chen@rokt.com>
2019-06-11 15:19:38 +1000

    Sprint 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,246 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+function Index() {
+  return (
+    <>
+      <h2>Dragons</h2>
+      <img src={process.env.PUBLIC_URL + '/img/image_3.png'} />
+    </>
+  );
+}
+
+function Images() {
+  // const images = [
+  //   {
+  //     url: '/img/image_1.png',
+  //     caption: ''
+  //   },
+  //   {
+  //     url: '/img/image_2.png',
+  //     caption: ''
+  //   }
+  // ];
+  return (
+    <>
+      <h2>Images</h2>
+
+      <img src={process.env.PUBLIC_URL + '/img/image_1.png'} />
+      <img src={process.env.PUBLIC_URL + '/img/image_2.png'} />
+    </>
+  );
+}
+
+function Diet() {
+  return (
+    <>
+      <h2>Diet</h2>
+      <p>
+        Dragons are obligate carnivores, with diets consisting entirely of meat. Dragons need to roast their prey with their fire-breath before consuming it - the only animals apart from humans who prefer cooked meat. Dragons can eat almost any kind of meat, anything from sheep to fish. Historical dragons ridden as beasts of war were known to eat fallen horses and even men on the battlefield. Fully grown dragons could swallow a live horse whole
+    </p>
+    </>);
+}
+
+function Reproduction() {
+  return (
+    <>
+      <h2>Reproduction</h2>
+      <p>
+      Since the reproductive system of dragons is internal, it can be tricky to determine if a dragon is a male or a female. The general behavior and subtle differences can provide information about the dragon's gender.
+
+1. Male dragons have slightly more muscular limbs, and/or thicker wings.
+
+2. Females dragons have slimmer muzzles (includes jaws, mouth, and nose).
+
+3. Females may have slightly bigger eyes.
+
+4. Male dragons have larger horns and broader arrowhead tails. The females' horns may be slightly shorter or slimmer, and the arrowhead portion of the tail end is more slender.
+
+5. Males may be depicted with a higher degree of aggression and impulsivity, as compared to female dragons. They might look more commanding. This doesn't mean females dragons have a refutable confidence. It is simply on a slightly quieter level.
+
+6. Females have an air of mysticism about them. Mysticism in the sense that a feeling of mystery and artistic wonder is inspired in us, when we are looking at a female dragon.
+
+
+In the mating process, the two partners fly high in the sky. When the time's right, they enfold each other in their wings and make free-fall. At less than a hundred feet from the ground, they open their wings and land. This process is due to the dragons' attraction to speed and risk, elements that are added to their loveplay.
+
+Western dragons lay eggs like reptiles and birds. The eggs are protected by a hard shell. The incubation period is unknown, or varies among species. The range is approximately one to five years, a lot faster than the 1000 years for Eastern dragons.
+
+
+      </p>
+    </>);
+}
+
+function Anatomy() {
+  return (
+    <>
+      <h2>Anatomy</h2>
+      <p>
+      1. Skeletal structure consists of a large head, long neck, broad shoulders, thick legs, strong tail and very large wings.
+
+2. Bones are very strong, hollow, and light, allowing for flight.
+
+3. Western dragons have stronger, more powerful muscles.
+
+4. Has the masseter muscle in the jaw that can easily crush bones to bits.
+
+5. Two large muscles near the chest area, are known as the Supraspinatus and Flexor alae major (the flight muscles).
+
+6. The huge, leathery wings usually seen on a Western dragon are similar to those of a bat.
+
+External Body
+
+All dragons are covered with scales.
+
+When born, a dragon's scales are as soft as tissue paper, and slowly harden while the dragon grows. The process of getting scales as tough as they can get takes about one year after being hatched.
+
+Some have horns or spikes on their heads and running down the spine. The eyes of the dragon also vary according to breed. Some have pupil-less eyes, while some have vivid purple eyes with pupils.
+
+The one most commonly associated with dragons, is a bright yellow eye with a serpent-like slit for a pupil, like the picture above.
+
+Smaller Eastern dragon species do not have scales. Instead, they are covered with leathery skin. Usually, they are brighly coloured.
+
+Wings of Western Dragons
+
+These wings are supported in four places to the body, as opposed to being supported in only two places like those of a bird.
+
+Such wings can carry more weight, such as the amount of forces to lift and maintain a dragon's body in flight. Eastern dragons fly by magical means.
+
+The wing is usually bigger than the dragon's body, to accommodate its mass.
+
+The bone structure of the wing is similar to a hand. It has a humerus, elbow joint, wrist, and phalanx, or fingers.
+
+The way a winged dragon actually flies is by scooping the air with the wings, and forcing itself upward. Baby dragons learn to fly in much the same way as baby birds. They jump from high spots, and learn to control themselves in flight.
+
+The wings can also be used as weapons, but since they aren't as tough and rugged as the rest of the body, this is a last resort. If the membranes of the wing are severed, the dragon may never fly again.
+
+Dragon Senses
+
+When it comes to the senses of a dragon, their senses are like a human's (sight, hearing, smelling, touch/balance, and taste), but much more extremely acute. Their regular five senses are incredibly sensitive.
+
+Some possess a sixth sense that enables them to "read" the emotions of another being. This sense is very accurate at close range.
+
+The most acute senses are those of sight, smell, and touch. Most Western dragons can see in a full colour range. Nocturnal species have enhanced night vision, with more rod cells.
+
+Fire Breath
+
+One of the dragon's most enduring legendary weapons is its ability to breathe fire.
+
+One suggestion for fire breath is that dragons have hydrogen glands (which help in flight). These glands also provide the fuel for fire-breathing. Dragons employ a spark from powdered platinum from ingested sediment rock. This reacts with the stored hydrogen to produce - fire!
+
+Another suggestion is in the way a dragon digests its food. The food is broken down into into organic materials such as methane. This gas is stored into a special kind of "lung" (the hydrocarbon storage vesicle) that holds the gas.
+
+Should the dragon (be sufficiently ticked off and) decide to unleash its fire-breathing qualities, methane will later be mixed with a small amount of phosphor that has the propriety to ignite in fire, at the contact of air.
+
+Some dragons in mythos can also breathe acid, frost and poison gas. And contrary to stereotypical belief, not all dragons are fire-breathers.
+
+Dragons would naturally be at the top of the food chain, due to their other available superior strengths. Fire breath is an added advantage.
+
+Dragon Scales
+
+i. As a Defence
+
+Many animals in nature use large eyespots to scare away potential predators. Young vulnerable dragons employ similar tactics. They flare their wings to appear more threatening.
+
+If scare tactics fail, their scaly armor offers them protection. The dragon's large body is covered in steel-hard scales, and works as an armor. Its softer, more sensitive body is protected. The scales can withstand potentially devestating damages, such as a direct sword-blow.
+
+ii. Scale Properties
+
+The scales, horns and claws of a dragon are made up of densely packed cells of a protein called keratin.
+
+Dragons inherit scale color from their parents the same way humans inherit hair color and texture. Scales can tell if a dragon is healthy or not. When a dragon is ill, the scales turn dull and muted. A healthy dragon has shiny scales.
+
+Scales are teardrop shaped, and lay staggered and overlapping. Chest scales are the largest. They have a squarish shape, and lay in "flaps". They run from the throat, down the belly, to the tail. The overlapping of the scales makes a slight scraping sound when the dragon walks.
+
+The dragon can make the scales stand up for washing, or as a cooling-off mechanism. Some breeds can change scale color to camouflage themselves. They may also change colors when agitated or during mating season. This is possible due to the pigment cells in the scales.
+
+
+Colors
+
+Scale color is determined by the genes of the parent dragons.
+
+Usually a dragon will mate with other dragons of a similar color. But if a mating takes place between a gold and a green dragon, the result might be a bronze offspring.
+
+Scales on a dragon are never just one shade. If a dragon is blue, there will be many shades of blue. Light, medium, dark, blue-black, etc. Scales are bright and shiny in a healthy dragon.
+
+It is impossible to list all the hues of a dragon's coloring (much like the color picker on computer graphics software), but they can be divided into three broad color groupings:
+
+scales
+Color gradient by the awesome D.R.A.G.O.N.S
+
+a) Blues
+b) Red
+c) Greens
+
+In general, scales are several hues from one of the main color categories, with a distinct metallic luster. When the scales have a pale, opaque appearance, it is a sure sign of ill health.
+
+Many dragons are known by their scales, such as Smaug the Golden.
+
+In the Pern series, by Anne McCaffrey, dragonhide has three essential layers that give it color: a gold layer, a brown layer, and a chromatic layer.
+
+References and Sources:
+
+> Here Be Dragons: Dragon Physiology
+> D.R.A.G.O.N.S: Advanced Dragon Description
+> Dragons' Anatomy, by Amie Granger
+> Discovery Channel, Dragons
+> Sommerland: Dragon Biology
+
+      </p>
+    </>);
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/images/">Image Gallery</Link>
+            </li>
+            <li>
+              <Link to="/diet/">Diet Information</Link>
+            </li>
+            <li>
+              <Link to="/reproduction/">Mating and Reproduction Information</Link>
+            </li>
+            <li>
+              <Link to="/anatomy/">Animal Anatomy and Biology</Link>
+            </li>
+            {/* <li>
+              <Link to="/behavior/">Animal Behavior</Link>
+            </li> */}
+            {/* <li>
+              <Link to="/habitat/">Animal Habitat Information</Link>
+            </li>
+            <li>
+              <Link to="/classification/">Scientific Name and Classification</Link>
+            </li>
+           
+          
+            <li>
+              <Link to="/anatomy/">Animal Anatomy and Biology</Link>
+            </li> */}
+          </ul>
+        </nav>
+
+        <Route path="/" exact component={Index} />
+        <Route path="/images/" component={Images} />
+        <Route path="/diet/" component={Diet} />
+        <Route path="/reproduction/" component={Reproduction} />
+        <Route path="/anatomy/" component={Anatomy} />
+        {/* <Route path="/behavior/" component={Behavior} /> */}
+
+        {/* <Route path="/habitat/" component={Habitat} />
+        <Route path="/classification/" component={Classification} />
+        */}
+      </div>
+    </Router>
   );
 }
 
